fd3c1b75e8dca2187bb0e6a78698ad76be9d56d1
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2024-12-05 22:12:02 -0300

    estudos

--- modulo 3/aula012/script.js
+++ modulo 3/aula012/script.js
@@ -1,7 +1,22 @@
-function soltou(e) {
-    console.log(e.code);
+var inputcampo = document.getElementById('campo')
+var lista = []
+var campolista = document.getElementById('lista')
+
+inputcampo.addEventListener('keyup', adicionar)
+
+function adicionar(e) {
+    if(e.code == 'Enter') {
+        if(inputcampo.value == '') {
+            alert('[ERRO] Digite algo primeiro.')
+        } else {
+            let newLi = document.createElement('li')
+            lista.push(inputcampo.value)
+            newLi.innerHTML = lista
+            campolista.appendChild(newLi)
+
+            console.log(lista)
+        }
+        inputcampo.value = ''
+        inputcampo.focus()
+    }
 }
-
-const input = document.querySelector('input')
-
-input.addEventListener('keyup', soltou)
