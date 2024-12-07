137cf885466395807260f6f9fdf13d8e06dee356
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2024-12-07 20:57:40 -0300

    estudo

--- modulo 4/aula003/script.js
+++ modulo 4/aula003/script.js
@@ -1,5 +1,42 @@
 class Person {
-    constructor(name) {
-        this.name = name
+
+    _passos = 0
+
+    constructor(nome, sobrenome, ano) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+        this.ano = ano
+    }
+
+    darPassos() {
+        this.passos++
+    }
+
+    get passo() {
+        return this._passos
+    }
+    
+    set passo(x) {
+        return this._passos = x
+    }
+
+    get nomeCompleto() {
+        return `${this.nome} ${this.sobrenome}`
     }
 }
+
+let pessoa1 = new Person('João', 'Porco', 19)
+let pessoa2 = new Person('Augusto', 'Brito', 22)
+let pessoa3 = new Person('Evelly', 'Brito', 20)
+
+
+pessoa1.passo = 2
+
+
+let botaopassos = document.getElementById('passo')
+botaopassos.addEventListener('click', () => {
+    pessoa1.darPassos()
+
+    console.log(`${pessoa1.nomeCompleto} agora tem ${pessoa1.ano} anos, e deu ${pessoa1.passo} passos.`)
+}) 
+
