bfbe5eaa8b703e7ecb4346b5508802657edcf151
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2024-12-28 23:31:07 -0300

    estudos

---
+++ modulo 6/aula007/script.js
@@ -0,0 +1,13 @@
+// Arredonda o valor para mais ou menos, com base no .5
+let arredonda = Math.round(4.77)
+// Arredonda o valor para menos
+let arredondabaixo = Math.floor(3.88)
+// Arredonda o valor para mais
+let arredondacima = Math.ceil(3.01)
+
+let numeroaleatorio = Math.floor(Math.random() * 100)
+
+console.log(arredonda)
+console.log(arredondabaixo)
+console.log(arredondacima)
+console.log(numeroaleatorio)
